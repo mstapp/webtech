2b1b46d1b59d970104bfda8c1deb0526df1099f4
Mike Stapp <mstapp@tiggertail.com>
2013-01-11 15:40:09 -0500

    Refactor to clean up code

--- app.js
+++ app.js
@@ -4,21 +4,36 @@ $(document).ready( function() {
     $('#ui').text('hello there');
 
 
-    var data = [
-        { id:1, name:'DaVinci' },
-        { id:2, name:'Michelangelo' },
-        { id:3, name:'Cezanne' },
-        { id:4, name:'Picasso' }
-    ];
+    var makeUI = function() {
+        var list = makeListElement(getRootUI());
+        showDataInList( getData(), list);
+    };
 
-    var list = $("<ul />");
-    list.appendTo("#ui");
+    var getRootUI = function() {
+        return $('#ui');
+    };
 
-    for (var i = 0; i < data.length; i++) {
-        $('<li />').text(data[i].name).appendTo(list);
-    }
+    var makeListElement = function(rootui) {
+        var list = $("<ul />");
+        list.appendTo(rootui);
+        return list;
+    };
 
+    var getData = function() {
+        return [
+            { id:1, name:'DaVinci' },
+            { id:2, name:'Michelangelo' },
+            { id:3, name:'Cezanne' },
+            { id:4, name:'Picasso' }
+        ];
+    };
 
+    var showDataInList = function(data, list) {
+        for (var i = 0; i < data.length; i++) {
+            $('<li />').text(data[i].name).appendTo(list);
+        }
+    };
 
+    makeUI();
 
 });
