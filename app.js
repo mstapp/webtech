e936933a8b62b619272350d8437954bc8df2f708
Mike Stapp <mstapp@tiggertail.com>
2013-01-18 15:24:29 -0500

    Refactor to ListView object

--- app.js
+++ app.js
@@ -3,44 +3,53 @@ $(document).ready( function() {
 
     $('#ui').text('hello there');
 
-    var makeUI = function() {
-        var list = makeListElement(getRootUI());
-        var callback = function(data) {
-            showDataInList( data, list);
-        }
-        getDataWithCallback(callback);
+    var ListMaker = function(){
+        this.foo = 1;
     };
 
-    var getRootUI = function() {
-        return $('#ui');
-    };
+    _.extend(ListMaker.prototype, {
 
-    var makeListElement = function(rootui) {
-        var list = $("<ul />");
-        list.appendTo(rootui);
-        return list;
-    };
+        makeUI: function() {
+            var list = this.makeListElement(this.getRootUI());
+            var self = this;
+            var callback = function(data) {
+                self.showDataInList( data, list);
+            }
+            this.getDataWithCallback(callback);
+        },
 
-    var getDataWithCallback = function(callback) {
-        // get JSON data from server, update view w/ data asynchronously
-        var url = '/data/artists.json';
-        console.log('get data from ' + url);
-        $.getJSON(url, function (result) {
-            console.log('REST call response data = ' + JSON.stringify(result));
-            // use data...
-            callback(result);
-        })
-        .error(function(jqXHR, textStatus, errorThrown) {
-            console.log('json error: ' + textStatus + ', error thrown: ' + errorThrown + ', text = ' + jqXHR.responseText);
-        });
-    };
+        getRootUI: function() {
+            return $('#ui');
+        },
 
-    var showDataInList = function(data, list) {
-        _.each(data, function(el) {
-            $('<li />').text(el.name).appendTo(list);
-        });
-    };
+        makeListElement: function(rootui) {
+            var list = $("<ul />");
+            list.appendTo(rootui);
+            return list;
+        },
 
-    makeUI();
+        getDataWithCallback: function(callback) {
+            // get JSON data from server, update view w/ data asynchronously
+            var url = '/data/artists.json';
+            console.log('get data from ' + url);
+            $.getJSON(url, function (result) {
+                console.log('REST call response data = ' + JSON.stringify(result));
+                // use data...
+                callback(result);
+            })
+            .error(function(jqXHR, textStatus, errorThrown) {
+                console.log('json error: ' + textStatus + ', error thrown: ' + errorThrown + ', text = ' + jqXHR.responseText);
+            });
+        },
+
+        showDataInList: function(data, list) {
+            _.each(data, function(el) {
+                $('<li />').text(el.name).appendTo(list);
+            });
+        },
+    });
+
+    var view = new ListMaker();
+    view.makeUI();
 
 });
