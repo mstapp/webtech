ba056c2031428c1511212a499f933a15816cb6d7
Mike Stapp <mstapp@tiggertail.com>
2013-01-23 21:26:06 -0500

    Refactor: extract Model, connect via events

--- app.js
+++ app.js
@@ -3,40 +3,28 @@ $(document).ready( function() {
 
     $('#ui').text('hello there');
 
-    var ListMaker = function(){
-        this.foo = 1;
+    var ListView = function(model){
+        this.model = model;
+        this.list = undefined;
     };
 
-    _.extend(ListMaker.prototype, {
-        dataType: 'artists',
-        list: undefined,
+    _.extend(ListView.prototype, {
 
+        initialize: function() {
+            this.model.on('reset', this.render, this);
+        },
 
         makeUI: function() {
             this.makeListElement(this.getRootUI());
-            this.fetchDataAndDisplay();
+            this.render();
         },
 
-        displayArtists: function() {
-            this.dataType = 'artists';
-            this.fetchDataAndDisplay();
-        },
-
-        displayCars: function() {
-            this.dataType = 'cars';
-            this.fetchDataAndDisplay();
+        render: function() {
+            this.showDataInList(model.getData(), this.list);
         },
 
 
         // private api
-
-        fetchDataAndDisplay: function() {
-            var self = this;
-            var callback = function(data) {
-                self.showDataInList(data, self.list);
-            }
-            this.getDataWithCallback(callback);
-        },
 
         getRootUI: function() {
             return $('#ui');
@@ -47,20 +35,6 @@ $(document).ready( function() {
             this.list.appendTo(rootui);
         },
 
-        getDataWithCallback: function(callback) {
-            // get JSON data from server, update view w/ data asynchronously
-            var url = '/data/' + this.dataType + '.json';
-            console.log('get data from ' + url);
-            $.getJSON(url, function (result) {
-                console.log('REST call response data = ' + JSON.stringify(result));
-                // use data...
-                callback(result);
-            })
-            .error(function(jqXHR, textStatus, errorThrown) {
-                console.log('json error: ' + textStatus + ', error thrown: ' + errorThrown + ', text = ' + jqXHR.responseText);
-            });
-        },
-
         showDataInList: function(data, list) {
             list.empty();
             _.each(data, function(el) {
@@ -69,17 +43,50 @@ $(document).ready( function() {
         },
     });
 
-    var view = new ListMaker();
+    var ListModel = function(){
+        this.dataType = 'artists';
+    };
+
+    _.extend(ListModel.prototype, Backbone.Events, {
+
+        fetch: function(callback) {
+            // get JSON data from server, update view w/ data asynchronously
+            var url = '/data/' + this.dataType + '.json';
+            console.log('get data from ' + url);
+            var self = this;
+            $.getJSON(url, function (result) {
+                console.log('REST call response data = ' + JSON.stringify(result));
+                self.data = result;
+                // notify observers that we've reset our state w/ new data
+                self.trigger('reset');
+            })
+            .error(function(jqXHR, textStatus, errorThrown) {
+                console.log('json error: ' + textStatus + ', error thrown: ' + errorThrown + ', text = ' + jqXHR.responseText);
+            });
+        },
+
+        getData: function() {
+            return this.data;
+        },
+
+        setDataType: function(type) {
+            this.dataType = type;
+        },
+
+    });
+
+    var model = new ListModel();
+    var view = new ListView(model);
+    view.initialize();
+
     view.makeUI();
+    model.fetch();
 
 
     $('#topics').on('change', function(e) {
         console.log(e.type + ' event from ' + e.target.value);
-        if (e.target.value === 'artists')
-            view.displayArtists();
-        else
-            view.displayCars();
-
+        model.setDataType(e.target.value);
+        model.fetch();
     });
 
 });
